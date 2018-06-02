47809aaf46b6f40e4d13cbba524283d838428bd3
vagrant <vagrant@homestead>
2018-06-02 14:03:55 +0000

    添加dingo

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,12 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+$app = app('Dingo\Api\Provider\LumenServiceProvider');
+$api->version('v1', function ($api) {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
 });
+//$app->register('Dingo\Api\Provider\LumenServiceProvider');
+//
+//$api->version('v1', function ($api) {
+//    $api->resource('users', 'UserController');
+//});
